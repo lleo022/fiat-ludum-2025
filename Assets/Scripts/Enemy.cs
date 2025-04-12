e051c3778d2024bb1fae0ec1d33f09a159e0b6f4
Leo Lai <leo22lai@gmail.com>
2025-04-12 16:03:39 -0700

    Added enemy attacks when player is close. Modified player layer.
    Have yet to add damage mechanics and enemy health.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class FunguyEnemy : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;  
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer;
-    
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         cooldownTimer += Time.deltaTime;
 
@@ -18,27 +27,27 @@ public class FunguyEnemy : MonoBehaviour
         {
             if (cooldownTimer >= attackCooldown)
             {
-                // Attack
+                cooldownTimer = 0;
+                anim.SetTrigger("melee");
             }
         }
     }
 
     private bool PlayerInSight()
     {
-        // RaycastHit2D hit  = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, 
-        //     boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
-        // return hit.collider != null;
-
-        return false;
+         RaycastHit2D hit  = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 
+             0, Vector2.left, 0, playerLayer);
+         
+        return hit.collider != null;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-      //  Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x,
-       //     boxCollider.bounds.size);
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+            boxCollider.bounds.size);
     }
-
 }
 
 
