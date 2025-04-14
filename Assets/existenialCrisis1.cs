c43b1cc7a0b7908eecdf923efe2af7b6d8901d1c
libozaza <aidansl113@gmail.com>
2025-04-14 15:54:34 -0700

    level 2

---
+++ Assets/existenialCrisis1.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using static UnityEngine.GraphicsBuffer;
+
+public class existentialCrisis1 : MonoBehaviour
+{
+    [SerializeField] GameLogic gameLogic;
+    [SerializeField] string[] monologue;
+    private bool done = false;
+
+    bool playerInRange = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(interact());
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DialogueScript dialogueScript = gameLogic.GetComponent<DialogueScript>();
+        if (dialogueScript == null)
+        {
+            Debug.LogError("DialogueScript component not found on GameLogic!");
+            return;
+        }
+    }
+    private IEnumerator interact()
+    {
+        if (!done)
+        {
+            done = true;
+            DialogueScript dialogueScript = gameLogic.GetComponent<DialogueScript>();
+
+            gameLogic.current_player.GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
+            gameLogic.current_player.GetComponent<Movement>().enabled = false;
+
+            dialogueScript.dialogue(monologue, "Statue");
+            yield return new WaitUntil(() => gameLogic.GetComponent<DialogueScript>().dialogueUI.activeSelf == false);
+            gameLogic.GetComponent<GameLogic>().current_player.GetComponent<Movement>().enabled = true;
+        }
+    }
+}
